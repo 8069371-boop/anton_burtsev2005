9d7ada86749d4d84a3e8b0b7021d596c6ee0796d
8069371-boop <8069371@stud.kai.edu.ua>
2025-11-23 23:47:07 +0200

    Update ITcpClient.cs

--- NetSdrClientApp/Networking/ITcpClient.cs
+++ NetSdrClientApp/Networking/ITcpClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,4 +16,4 @@ namespace NetSdrClientApp.Networking
         event EventHandler<byte[]> MessageReceived;
         public bool Connected { get; }
     }
-}
+}  
