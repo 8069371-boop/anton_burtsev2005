367836eb2e6a8aa09e06b229753d51c33ffbebce
8069371-boop <8069371@stud.kai.edu.ua>
2025-11-24 01:04:05 +0200

    Update TcpClientWrapper.cs

--- EchoTcpServer/Abstractions/TcpClientWrapper.cs
+++ EchoTcpServer/Abstractions/TcpClientWrapper.cs
@@ -6,25 +6,45 @@ namespace EchoServer.Abstractions
     public class TcpClientWrapper : ITcpClientWrapper
     {
         private readonly TcpClient _client;
+        private bool _disposed;
 
         public TcpClientWrapper(TcpClient client)
         {
-            _client = client;
+            _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
         public INetworkStreamWrapper GetStream()
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(TcpClientWrapper));
+            
             return new NetworkStreamWrapper(_client.GetStream());
         }
 
         public void Close()
         {
+            if (_disposed)
+                return;
+            
             _client.Close();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _client?.Close();
+                    _client?.Dispose();
+                }
+                _disposed = true;
+            }
         }
 
         public void Dispose()
         {
-            _client?.Dispose();
+            Dispose(true);
             GC.SuppressFinalize(this);
         }
     }
