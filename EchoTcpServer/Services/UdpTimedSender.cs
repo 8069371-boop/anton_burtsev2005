9aef1f30c13ba04e65dd0385385385a8e484d765
8069371-boop <8069371@stud.kai.edu.ua>
2025-11-24 01:06:27 +0200

    Update UdpTimedSender.cs

--- EchoTcpServer/Services/UdpTimedSender.cs
+++ EchoTcpServer/Services/UdpTimedSender.cs
@@ -16,6 +16,7 @@ namespace EchoServer.Services
         private readonly UdpClient _udpClient;
         private Timer? _timer;
         private ushort _counter = 0;
+        private bool _disposed;
         
         // S2245: Random is used only for generating test data payload, not for any security-sensitive operations
 #pragma warning disable S2245
@@ -24,7 +25,7 @@ namespace EchoServer.Services
 
         public UdpTimedSender(string host, int port, ILogger logger)
         {
-            _host = host;
+            _host = host ?? throw new ArgumentNullException(nameof(host));
             _port = port;
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _udpClient = new UdpClient();
@@ -32,19 +33,25 @@ namespace EchoServer.Services
 
         public void StartSending(int intervalMilliseconds)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(UdpTimedSender));
+            
             if (_timer != null)
                 throw new InvalidOperationException("Sender is already running.");
+            
             _timer = new Timer(SendMessageCallback, null, 0, intervalMilliseconds);
         }
 
         private void SendMessageCallback(object? state)
         {
+            if (_disposed)
+                return;
+            
             try
             {
                 byte[] samples = new byte[1024];
                 _random.NextBytes(samples);
                 _counter++;
-
                 byte[] msg = (new byte[] { 0x04, 0x84 })
                     .Concat(BitConverter.GetBytes(_counter))
                     .Concat(samples)
@@ -62,19 +69,31 @@ namespace EchoServer.Services
 
         public void StopSending()
         {
+            if (_disposed)
+                return;
+            
             _timer?.Dispose();
             _timer = null;
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _timer?.Dispose();
+                    _timer = null;
+                    _udpClient?.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
         public void Dispose()
         {
-            StopSending();
-            _udpClient.Dispose();
+            Dispose(true);
             GC.SuppressFinalize(this);
         }
     }
 }
-
-
-
-
