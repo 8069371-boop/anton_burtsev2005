789a089e398c1c4f8b03e1606ed2909704ace6c2
8069371-boop <8069371@stud.kai.edu.ua>
2025-11-24 13:57:48 +0200

    Update NetworkStreamWrapper.cs

--- EchoTcpServer/Abstractions/NetworkStreamWrapper.cs
+++ EchoTcpServer/Abstractions/NetworkStreamWrapper.cs
@@ -8,45 +8,27 @@ namespace EchoServer.Abstractions
     public class NetworkStreamWrapper : INetworkStreamWrapper
     {
         private readonly NetworkStream _stream;
-        private bool _disposed;
 
         public NetworkStreamWrapper(NetworkStream stream)
         {
-            _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            _stream = stream;
         }
 
         public Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken token)
         {
-            if (_disposed)
-                throw new ObjectDisposedException(nameof(NetworkStreamWrapper));
-            
             return _stream.ReadAsync(buffer, offset, count, token);
         }
 
         public Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken token)
         {
-            if (_disposed)
-                throw new ObjectDisposedException(nameof(NetworkStreamWrapper));
-            
             return _stream.WriteAsync(buffer, offset, count, token);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    _stream?.Dispose();
-                }
-                _disposed = true;
-            }
         }
 
         public void Dispose()
         {
-            Dispose(true);
+            _stream?.Dispose();
             GC.SuppressFinalize(this);
         }
     }
 }
+
