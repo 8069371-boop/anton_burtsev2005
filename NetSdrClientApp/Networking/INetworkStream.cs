042dc320a2c359282d5fe79c3c0d4312f25e8365
8069371-boop <8069371@stud.kai.edu.ua>
2025-11-24 14:11:27 +0200

    Update INetworkStream.cs

--- NetSdrClientApp/Networking/INetworkStream.cs
+++ NetSdrClientApp/Networking/INetworkStream.cs
@@ -1,1 +1,14 @@
-INetworkStream.cs
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetSdrClientApp.Networking.Interfaces
+{
+    public interface INetworkStream : IDisposable
+    {
+        Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken token);
+        Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken token);
+    }
+}
+
+
